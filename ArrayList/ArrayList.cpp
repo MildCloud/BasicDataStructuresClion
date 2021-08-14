08d0c4cda5eadc301b5ce70b82dfc942d09a3ce5
tian-shu <tian-shu@sjtu.edu.cn>
2021-08-14 11:25:00 +0800

    Link List

--- ArrayList/ArrayList.cpp
+++ ArrayList/ArrayList.cpp
@@ -24,21 +24,33 @@ int find (int findElement, list list) {
     return -1;
 }
 
-//insert newElement in i (i >= 0 && i <= last)
+//insert newElement in array[i] (i >= 0 && i <= last)
 void insert (int newElement, int i, list list) {
+    for (int j = list->last; j >= i; j--) {
+        list->array[j+1] = list->array[j];
+    }
     list->last++;
     list->array[i] = newElement;
 }
 
+//delete an element in array[i] (i >= 0 && i <= last)
+void deleteL (int i, list list) {
+    for (int j = i; j <= list->last; j++) {
+        list->array[j] = list->array[j+1];
+    }
+    list->last--;
+}
+// Be caerful when using "j" an "i"
+
 int main () {
-    int findElement;
+    int findElement, insertElement, insertPos, deletePos;
     listNode lNode;
     list lPointer = &lNode;
 
     cout << "last is " << lNode . last << endl;
 
     for (int i=0; i < 10; i++) {
-        insert((i+10), i, lPointer);
+        insert ((i+10), i, lPointer);
         cout << "last is " << lPointer -> last << endl;
     }
 
@@ -49,5 +61,24 @@ int main () {
     cout << "find the following element: " << endl;
     cin >> findElement;
     cout << "This element is in array[" << find (findElement, lPointer) << "]." << endl;
+
+    cout << "Insert a new elment:" << endl;
+    cin >> insertElement;
+    cout << "in: " <<endl;
+    cin >> insertPos;
+    insert (insertElement, insertPos, lPointer);
+
+    for (int i=0; i <= lPointer->last; i++) {
+        cout << "array[" << i << "] = " << lPointer -> array[i] << endl;
+    }
+
+    cout << "Delet a element in the following position: " << endl;
+    cin >> deletePos;
+    deleteL (deletePos, lPointer);
+
+    for (int i=0; i <= lPointer->last; i++) {
+        cout << "array[" << i << "] = " << lPointer -> array[i] << endl;
+    }
+
     return 0;
 }
